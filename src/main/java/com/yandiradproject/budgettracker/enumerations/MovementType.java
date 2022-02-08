30907d086ffbc69193082dcc09c61187c0c41638
yandirad <yandirad@gmail.com>
2022-02-08 16:07:38 +0100

    add codes to enums

--- src/main/java/com/yandiradproject/budgettracker/enumerations/MovementType.java
+++ src/main/java/com/yandiradproject/budgettracker/enumerations/MovementType.java
@@ -6,13 +6,19 @@ package com.yandiradproject.budgettracker.enumerations;
  * @author yandirad
  */
 public enum MovementType {
-    FIXED_EXPENSE("Fixed Expense"), VARIABLE_EXPENSE("Variable Expense"), 
-    REGULAR_INCOME("Regular Income"), IRREGULAR_INCOME("Irregular Income"), SAVING("Saving");
+    FIXED_EXPENSE(0, "Fixed Expense"), VARIABLE_EXPENSE(1, "Variable Expense"), 
+    REGULAR_INCOME(3, "Regular Income"), IRREGULAR_INCOME(4, "Irregular Income"), SAVING(5, "Saving");
     
     private String value;
+    private Integer code;
     
-    private MovementType(String value){
+    private MovementType(Integer code, String value){
+        this.code = code;
         this.value = value;
+    }
+    
+    public Integer getCode(){
+        return code;
     }
 
     public String getValue() {
