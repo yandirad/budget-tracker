c0ba1cd079cc7a4bdf172227f094189bc3bfd6ae
yandirad <yandirad@gmail.com>
2022-02-18 11:09:37 +0100

    add create and edit methods

--- src/main/java/com/yandiradproject/budgettracker/services/MovementService.java
+++ src/main/java/com/yandiradproject/budgettracker/services/MovementService.java
@@ -1,6 +1,13 @@
 
 package com.yandiradproject.budgettracker.services;
 
+import com.yandiradproject.budgettracker.entities.Movement;
+import com.yandiradproject.budgettracker.entities.User;
+import com.yandiradproject.budgettracker.enumerations.MovementType;
+import com.yandiradproject.budgettracker.repositories.MovementRepository;
+import java.util.Date;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -10,6 +17,54 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MovementService {
-
+    
+    @Autowired
+    private MovementRepository movementRepository;
+    
+    public Movement createMovement(String name, Date createdAt, Integer type, Double amount, String userId) 
+            throws Exception{
+        Movement movement = new Movement();
+        
+        Validator validator = new Validator();
+        validator.stringValidate(name, "Name");
+        validator.dateValidate(createdAt, "Date");
+        validator.doubleValidate(amount, "Amount");
+        validator.movementTypeValidate(type, "Movement Type");
+        
+        MovementType mt = MovementType.values()[type];
+        
+        movement.setName(name);
+        movement.setType(mt);
+        movement.setAmount(amount);
+        movement.setCreatedAt(createdAt);
+        
+        movementRepository.save(movement);
+        
+        return movement;
+    }
+    
+    public Movement editMovement(String id, String name, Date createdAt, Integer type, Double amount) 
+            throws Exception{
+        
+        Optional<Movement> movementOpt = movementRepository.findById(id);
+        
+        Validator validator = new Validator();
+        validator.stringValidate(name, "Name");
+        validator.dateValidate(createdAt, "Date");
+        validator.doubleValidate(amount, "Amount");
+        validator.movementTypeValidate(type, "Movement Type");
+        
+        MovementType mt = MovementType.values()[type];
+        Movement movement = movementOpt.get();
+        
+        movement.setName(name);
+        movement.setCreatedAt(createdAt);
+        movement.setType(mt);
+        movement.setAmount(amount);
+        
+        movementRepository.save(movement);
+        return movement;
+        
+    }
 
 }
--- src/main/java/com/yandiradproject/budgettracker/services/UserService.java
+++ src/main/java/com/yandiradproject/budgettracker/services/UserService.java
@@ -1,5 +1,13 @@
 package com.yandiradproject.budgettracker.services;
 
+import com.yandiradproject.budgettracker.entities.Movement;
+import com.yandiradproject.budgettracker.entities.User;
+import com.yandiradproject.budgettracker.repositories.UserRepository;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -9,6 +17,47 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+        public User createUser(String name, Date createdAt, String pass) 
+            throws Exception{
+            
+            User user = new User();
+
+            Validator validator = new Validator();
+            validator.stringValidate(name, "Name");
+            validator.dateValidate(createdAt, "Date");
+
+            List<Movement> movementsList = new ArrayList();
+
+            user.setName(name);
+            user.setCreatedAt(createdAt);
+            user.setPass(pass);
+            user.setMovements(movementsList);
+            return user;
+    }
+    
+    public User editUser(String id, String name, List<Movement> movements, Date createdAt, String pass) 
+            throws Exception{
+        
+        Optional<User> userOpt = userRepository.findById(id);
+        
+        Validator validator = new Validator();
+        validator.stringValidate(name, "Name");
+        validator.dateValidate(createdAt, "Date");
+        validator.passValidate(pass, "Pass");
+        
+        User user = userOpt.get();
+        
+        user.setName(name);
+        user.setMovements(movements);
+        user.setCreatedAt(createdAt);
+        user.setPass(pass);
+        
+        return user;  
+    }
 
 
 }
